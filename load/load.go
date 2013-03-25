f65097e8fa171f69c7dd661507dceca36b0d7102
Scott Cagno <scottiecagno@gmail.com>
2013-03-25 16:25:15 -0400

    started new libs

---
+++ load/load.go
@@ -0,0 +1,7 @@
+// -------
+// load.go ::: load balancer
+// -------
+// Copyright (c) 2013-Present, Scott Cagno. All rights reserved.
+// This source code is governed by a BSD-style license.
+
+package load
---
+++ prox/prox.go
@@ -0,0 +1,7 @@
+// -------
+// prox.go ::: proxy server (reverse)
+// -------
+// Copyright (c) 2013-Present, Scott Cagno. All rights reserved.
+// This source code is governed by a BSD-style license.
+
+package prox
