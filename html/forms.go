55fb14bff0a8192572c9dc9da1e83accfa517b1b
Greg Pechiro <gregpechiro@yahoo.com>
2013-10-23 17:37:02 -0400

    first commit rc2

---
+++ html/forms.go
@@ -0,0 +1,260 @@
+// *
+// * -----------
+// * forms.go ::: html form generator and validator
+// * -----------
+// * Copyright (c) 2013-Present, Scott Cagno & Greg Pechiro. All rights reserved.
+// * BSD-style license can be found at https://sites.google.com/site/bsdc3license
+// *
+
+package html
+
+import (
+	"html/template"
+	"net/http"
+	"strings"
+	"bytes"
+	"fmt"
+)
+
+// form structure
+type Form struct {
+	Header		string
+	Action	 	string
+	Inputs 		[]*Input
+	Submit		*Button
+	Errors		map[string]string 
+	t 			*template.Template
+}
+
+// initialize form struct
+func InitForm(action, header string, submit *Button) *Form {
+	return &Form{
+		Action	: action,
+		Header  : header,
+		Submit 	: submit,
+		t 		: template.Must(template.New("form").Parse(DEFAULT_FORM)),
+	}
+}
+
+// add input to form struct
+func (self *Form) Add(i *Input) {
+	self.Inputs = append(self.Inputs, i)
+}
+
+/*
+	if !form.IsValid(r) {
+		ts.Render(w, "example.html", html.M{"form":form.Render()})
+	} else {
+		// form is valid, db call for user
+		...
+		if ok {
+			// continue
+		} else {
+			// session is dead
+			http.Redirect(w, r, "/example", 303)
+		}
+	}
+*/
+
+// form validator
+func (self *Form) IsValid(r *http.Request) bool {
+	validState := true
+	for i := 0; i < len(self.Inputs); i++ {
+		formVal := r.FormValue(self.Inputs[i].Name)
+		if self.Inputs[i].Type != "password" {
+			formVal = clean(formVal)
+		}
+		if self.Inputs[i].Required {
+			if len(formVal) < 1 {
+				err := fmt.Sprintf("*%s is a required field!", self.Inputs[i].Holder)
+				self.Inputs[i].Err = err
+				self.Errors[self.Inputs[i].Name] = err
+				validState = false
+			} else {
+				self.Inputs[i].Value = formVal
+			}
+		}
+		switch self.Inputs[i].Type {
+		case "email":
+			if strings.Count(formVal, "@") != 1 {
+				err := fmt.Sprintf("*%s requires an email address!", self.Inputs[i].Holder)
+				self.Inputs[i].Err = err
+				self.Errors[self.Inputs[i].Name] = err
+				validState = false
+			} else {
+				self.Inputs[i].Value = formVal
+			}
+		case "password":
+			if strings.ContainsAny(formVal, ";, \n\r\t") {
+				err := fmt.Sprintf("*%s contains invalid characters!", self.Inputs[i].Holder)
+				self.Inputs[i].Err = err
+				self.Errors[self.Inputs[i].Name] = err
+				validState = false
+			} else {
+				if self.Inputs[i].Min != 0 && len(formVal) < self.Inputs[i].Min {
+					err := fmt.Sprintf("*%s minimum is %d!", self.Inputs[i].Holder, self.Inputs[i].Min)
+					self.Inputs[i].Err = err
+					self.Errors[self.Inputs[i].Name] = err
+					validState = false
+				} else {
+					self.Inputs[i].Value = formVal
+				}
+				if self.Inputs[i].Max != 0 && self.Inputs[i].Max > self.Inputs[i].Min && len(formVal) > self.Inputs[i].Max {
+					err := fmt.Sprintf("*%s maximum is %d!", self.Inputs[i].Holder, self.Inputs[i].Max)
+					self.Inputs[i].Err = err
+					self.Errors[self.Inputs[i].Name] = err
+					validState = false
+				} else {
+					self.Inputs[i].Value = formVal
+				}
+			}
+		case "number":
+			if !isNumber(formVal) {
+				err := fmt.Sprintf("*%s requires a number!", self.Inputs[i].Holder)
+				self.Inputs[i].Err = err
+				self.Errors[self.Inputs[i].Name] = err
+				validState = false
+			} else {
+				self.Inputs[i].Value = formVal
+			}
+		}
+	}
+	return validState
+}
+
+// render form
+func (self *Form) Render() string {
+	var html bytes.Buffer
+	self.t.Execute(&html, self)
+	return html.String()
+}
+
+// sanitize
+func clean(s string) string {
+	return strings.Replace(strings.Replace(strings.Join(strings.Fields(s), " "), ",","", -1), ";","", -1)
+}
+
+// check is string is a number
+func isNumber(s string) bool {
+	ok := true
+	for _, b := range []byte(s) {
+		if !('0' <= b && b <= '9') {
+			ok = false
+		}
+	}
+	return ok
+}
+
+// input structure
+type Input struct {
+	Name, Type, Value, Holder, Err, Class 	string
+	Required								bool
+	Max, Min								int
+}
+
+// text input
+func TextInput(name, holder string) *Input {
+	return InitInput("text", name, holder)
+}
+
+// number input
+func NumberInput(name, holder string) *Input {
+	return InitInput("number", name, holder)
+}
+ 
+// email input
+func EmailInput(name, holder string) *Input {
+	return InitInput("email", name, holder)
+}
+// password input
+func PassInput(name, holder string) *Input {
+	return InitInput("password", name, holder)
+}
+// date input
+func DateInput(name, holder string) *Input {
+	return InitInput("date", name, holder)
+}
+// hidden input
+func HiddenInput(name, holder string) *Input {
+	return InitInput("hidden", name, holder)
+}
+
+// initialize input struct
+func InitInput(typ, name, holder string) *Input {
+	return &Input{
+		Type 		: typ,
+		Name 		: name,
+		Holder 		: holder,
+	}
+}
+
+// class
+func (self *Input) SetClass(c string) *Input {
+	self.Class = c
+	return self
+}
+
+// max
+func (self *Input) SetMax(n int) *Input {
+	self.Max = n
+	return self
+}
+
+// min
+func (self *Input) SetMin(n int) *Input {
+	self.Min = n
+	return self
+}
+
+// required
+func (self *Input) SetRequired() *Input {
+	self.Required = true
+	return self
+}
+
+// button structure
+type Button struct {
+	Icon, Color, Name 	string
+}
+
+// initialize button struct
+func InitButton(icon, color, name string) *Button {
+	return &Button{
+		Icon 	: icon,
+		Color 	: color,
+		Name	: name,
+	}
+}
+
+const (
+	WHITE		= "btn-default"
+	BLUE 		= "btn-primary"
+	GREEN 		= "btn-success"
+	LIGHT_BLUE 	= "btn-info"
+	ORANGE 		= "btn-warning"
+	RED 		= "btn-danger"
+	LINK 		= "btn-link"
+)
+
+// default template
+var DEFAULT *template.Template
+var DEFAULT_FORM=`<div class="col-sm-12">
+	<form class="form form-horizontal" role="form" method="post" action="{{.Action}}">
+		{{if .Header}}<legend class="col-sm-12">{{.Header}}</legend>{{end}}
+		{{range .Inputs}}
+		<div class="form-group line">
+			<div class="col-sm-12">
+				{{if .Err}}<label class="text-danger text-left">{{.Err}}</label>{{end}}
+				<input name="{{.Name}}" type="{{.Type}}" placeholder="{{.Holder}}" {{if .Value}}value="{{.Value}}"{{end}} class="form-control input-sm{{if .Class}}{{.Class}}{{end}}" {{if .Min}}min="{{.Min}}"{{end}} {{if .Max}}max="{{.Max}}"{{end}} {{if .Required}}required{{end}}>
+			</div>
+		</div>
+		{{end}}
+		<div class="form-group">
+			<div class="col-sm-12">
+				<button type="submit" class="btn {{.Submit.Color}}">
+					<i class="{{.Submit.Icon}}"></i> {{.Submit.Name}}
+				</button>
+			</div>
+		</div>
+	</form>
+</div>`
---
+++ html/template.go
@@ -0,0 +1,124 @@
+// *
+// * ---------------
+// * template.go ::: template store
+// * ---------------
+// * Copyright (c) 2013-Present, Scott Cagno & Greg Pechiro. All rights reserved.
+// * BSD-style license can be found at https://sites.google.com/site/bsdc3license
+// *
+
+package html
+
+import (
+	"html/template"
+	"net/http"
+	"reflect"
+	"strings"
+	"sync"
+	"sort"
+	"fmt"
+)
+
+// map type
+type M map[string]interface{}
+
+// template
+type TemplateStore struct {
+	dir    string
+	base   string
+	cached map[string]*template.Template
+	funcs  template.FuncMap
+	mu     sync.Mutex
+}
+
+// return a new template store instace
+func InitTemplateStore(dir, base string) *TemplateStore {
+	return &TemplateStore{
+		dir:    dir,
+		base:   base,
+		cached: make(map[string]*template.Template),
+		funcs: template.FuncMap{
+			"title"	: 	strings.Title,
+			"safe"	: 	safe,
+			"eq"	: 	eq,
+			"add"	: 	add,
+			"sub"	:	sub,
+			"decr"	:	decr,
+			"incr"	:	incr,
+			"split"	:	strings.Split,
+			"sort"  :	sorter,
+		},
+	}
+}
+
+// sorts a string slice
+func sorter(s []string) []string {
+	sort.Strings(s)
+	return s
+}
+
+// html safe escaper
+func safe(html string) template.HTML {
+	return template.HTML(html)
+}
+
+// check for equality
+func eq(args ...interface{}) bool {
+	if len(args) == 0 {
+		return false
+	}
+	x := args[0]
+	switch x := x.(type) {
+	case string, int, int64, byte, float32, float64:
+		for _, y := range args[1:] {
+			if x == y {
+				return true
+			}
+		}
+		return false
+	}
+
+	for _, y := range args[1:] {
+		if reflect.DeepEqual(x, y) {
+			return true
+		}
+	}
+	return false
+}
+
+// decrement
+func decr(a int) int { return a - 1 }
+
+// increment
+func incr(a int) int { return a + 1 }
+
+// add
+func add(a, b int) int { return a + b }
+
+// subtract
+func sub(a, b int) int { return a - b }
+
+// load template files associated with base into cache
+func (self *TemplateStore) Load(name ...string) {
+	self.mu.Lock()
+	defer self.mu.Unlock()
+	for i := 0; i < len(name); i++ {
+		t := template.New(self.base).Funcs(self.funcs)
+		t = template.Must(t.ParseFiles(self.dir+"/"+self.base, self.dir+"/"+name[i]))
+		self.cached[name[i]] = t
+	}
+}
+
+// render a template by name
+func (self *TemplateStore) Render(w http.ResponseWriter, name string, m interface{}) {
+	self.cached[name].Execute(w, m)
+}
+
+// render raw data
+func (self *TemplateStore) Raw(w http.ResponseWriter, format string, a ...interface{}) {
+	fmt.Fprintf(w, format, a...)
+}
+
+// set the header content type
+func (self *TemplateStore) ContentType(w http.ResponseWriter, typ string) {
+	w.Header().Set("Content Type", typ)
+}
